317d7a3e79b7b81ea30057f289dd29013d4f90c0
Jennifer Gibbs <jennegibbs@gmail.com>
2025-09-29 15:41:14 +1000

    Added timezones for each of cities in the citiesContainer. When the select changes the city, run a function to change the city, remove the contents of the cityContainer and get the date and timezones for the selected city

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function updateTime() {
   //Paris
   let parisElement = document.querySelector("#paris");
   let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = paris.querySelector(".time");
+  let parisTimeElement = parisElement.querySelector(".time");
   let parisTime = moment().tz("Europe/Paris");
 
   parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
@@ -33,3 +33,24 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let citiesTimeZone = event.target.value;
+  let citiesName = citiesTimeZone.replace("_", " ").split("/")[1];
+  citiesTime = moment().tz(citiesTimeZone);
+  //console.log(citiesTime.format("MMMM Do, YYYY"));
+  let cityContainerElement = document.querySelector("#cityContainer");
+  cityContainerElement.innerHTML = `
+ <div class="city">
+          <div>
+            <h2>${citiesName}</h2>
+          <div class="date">${citiesTime.format("MMMM Do, YYYY")}</div>
+        </div>
+          <div class="time">${citiesTime.format(
+            "h:mm:ss"
+          )}<small>${citiesTime.format("A")}</small></div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#cities");
+citiesSelectElement.addEventListener("change", updateCity);
